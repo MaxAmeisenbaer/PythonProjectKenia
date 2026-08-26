65716dd9f878d3f501edfcdf839eacd4e06ec173
MaxAmeisenbaer <max.maeder03@gmail.com>
2026-08-26 11:41:40 +0200

    change/benchmark_config/exclude_nit_data

--- benchmark_szenario_sha.py
+++ benchmark_szenario_sha.py
@@ -5,9 +5,9 @@ def get_benchmark_config():
     measurements = {
         "SHA": ["disch", "doc", "elc", "nit", "tcd", "toc", "tsp", "tur", "wl", "prec"],
         "WSH": ["dir", "ec15", "gust", "par", "rh", "stemp15", "temp", "vwc15", "wind", "prec"],
-        "TTP": ["disch", "doc", "elc", "nit", "tcd", "toc", "tsp", "tur", "wl", "prec"],
+        "TTP": ["disch", "doc", "elc", "tcd", "toc", "tsp", "tur", "wl", "prec"],
         "TF2": ["temp", "prec"],
-        "NF": ["disch", "doc", "elc", "nit", "tcd", "toc", "tsp", "tur", "wl", "prec"],
+        "NF": ["disch", "doc", "elc", "tcd", "toc", "tsp", "tur", "wl", "prec"],
         "Kur": ["prec"],
         "KFS": ["dir", "gust", "par", "rh", "stemp15", "temp", "vwc15", "wind"],
         "Fun": ["prec"],
